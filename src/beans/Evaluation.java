e9ee30df87dc25d2c6daa9a0971a85fb36bd910a
EL KHATTAB_ikram <ikramelkhattab0511@gmail.com>
2025-03-12 17:54:57 +0100

    Update Evaluation.java

--- src/beans/Evaluation.java
+++ src/beans/Evaluation.java
@@ -10,14 +10,14 @@ package beans;
  * @author hp
  */
 public class Evaluation {
-    private int id;
-    private int enseignant_id;
-    private int etudiant_id;
+   
+    private Enseignant enseignant_id;
+    private Etudiant etudiant_id;
     private double note;
     private String commentaire;
      
-   public Evaluation(int id, int enseignant_id, int etudiant_id, double note, String commentaire) {
-        this.id = id;
+   public Evaluation( Enseignant enseignant_id, Etudiant etudiant_id, double note, String commentaire) {
+       
         this.enseignant_id = enseignant_id;
         this.etudiant_id = etudiant_id;
         this.note = note;
@@ -25,44 +25,30 @@ public class Evaluation {
     } 
 
     /**
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
      * @return the enseignant_id
      */
-    public int getEnseignant_id() {
+    public Enseignant getEnseignant_id() {
         return enseignant_id;
     }
 
     /**
      * @param enseignant_id the enseignant_id to set
      */
-    public void setEnseignant_id(int enseignant_id) {
+    public void setEnseignant_id(Enseignant enseignant_id) {
         this.enseignant_id = enseignant_id;
     }
 
     /**
      * @return the etudiant_id
      */
-    public int getEtudiant_id() {
+    public Etudiant getEtudiant_id() {
         return etudiant_id;
     }
 
     /**
      * @param etudiant_id the etudiant_id to set
      */
-    public void setEtudiant_id(int etudiant_id) {
+    public void setEtudiant_id(Etudiant etudiant_id) {
         this.etudiant_id = etudiant_id;
     }
 
@@ -93,5 +79,7 @@ public class Evaluation {
     public void setCommentaire(String commentaire) {
         this.commentaire = commentaire;
     }
+ 
+    
    
 }
